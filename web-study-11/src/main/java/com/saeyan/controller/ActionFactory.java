741d2f88d93a24c3679ce58a5d39e01d377bf156
uno-km <zhfldk014745@naver.com>
2021-07-07 19:58:16 +0900

    commit;

--- web-study-11/src/main/java/com/saeyan/controller/ActionFactory.java
+++ web-study-11/src/main/java/com/saeyan/controller/ActionFactory.java
@@ -21,25 +21,25 @@ public class ActionFactory {
 			action = new BoardListAction();
 		} else if (command.equals("board_write_form")) {
 			action = new BoardWriteFormAction();
-			
+
 		} else if (command.equals("board_view")) {
 			action = new BoardViewAction();
-			
+
 		} else if (command.equals("board_check_pass_form")) {
 			action = new BoardCheckPassFormAction();
-			
+
 		} else if (command.equals("board_check_pass")) {
 			action = new BoardCheckPassAction();
-			
+
 		} else if (command.equals("board_update_form")) {
 			action = new BoardUpdateFormAction();
-			
+
 		} else if (command.equals("board_update")) {
 			action = new BoardUpdateAction();
-			
+
 		} else if (command.equals("board_delete")) {
 			action = new BoardDeleteAction();
-			
+
 		} else if (command.equals("board_write")) {
 			action = new BoardWriteAction();
 		}
